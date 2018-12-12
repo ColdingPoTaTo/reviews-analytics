5f847452386f790d1530ccfdfc37c7519bb15a9e
xiao will <william.9150@gmail.com>
2018-12-13 07:35:51 +0800

    add 100

--- read.py
+++ read.py
@@ -4,8 +4,8 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
-			print(len(data))
+		#if count % 1000 == 0:
+			#print(len(data))
 print(len(data))
 
 sum_len = 0
@@ -13,3 +13,8 @@ for d in data:
 	sum_len += len(d)
 print('每一筆留言的平均長度為', sum_len/len(data))
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
